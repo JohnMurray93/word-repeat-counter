2e4721bd7756fb685e15434f06d8f0e48e404c66
Daniel Cheng <danielchengml@gmail.com>
2017-10-20 13:13:49 -0700

    update 2-page interface and homecontroller. Upon testing the RemovePunctuation() method does not work.

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -12,6 +12,19 @@ namespace WordCounter.Controllers
     {
       return View();
     }
+    [HttpPost("/results")]
+    public ActionResult WordCounter()
+    {
+      RepeatCounter userInput = new RepeatCounter(Request.Form["wordInput"], Request.Form["sentenceInput"]);
+      string Word = userInput.GetWord();
+      string Sentence = userInput.GetSentence();
+      int repeats = userInput.CountRepeats();
+      string Repeats = repeats.ToString();
+
+      Dictionary<string, string> WordCount = new Dictionary<string, string> () {{"Word", Word}, {"Sentence", Sentence}, {"Repeats", Repeats}};
+
+      return View(WordCount);
+    }
   }
 
 }
